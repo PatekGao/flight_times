9420967a46508d5fb9b0c816dc5579613d01256f
PatekGao <1315587032@qq.com>
2025-03-05 13:28:59 +0800

    [feat]完成DAY 1 = DAY 2 【0-10点】出港航班*60%（国内国际先都用这个比例）。在pre_task2中实现DAY1航班的添加，（60%四舍五入）寻找DAY2最迟的国内国际航班量并修改为DAY1加入到最前部分

--- OR/pre_task2.py
+++ OR/pre_task2.py
@@ -55,6 +55,33 @@ full_df = pd.DataFrame(records)[['ID', '时间', '市场', '进出港', '机型'
 arr_df = full_df[full_df['进出港'] == 'ARR']
 dep_df = full_df[full_df['进出港'] == 'DEP']
 
+# 统计日期为2的0-10点出港航班数量
+dep_flights_2_0_10 = dep_df[(dep_df['日期'] == 2) & (dep_df['时间'].between('00:00', '10:00'))].shape[0]
+
+# 国际出港航班数量
+international_flights = \
+    dep_df[(dep_df['日期'] == 2) & (dep_df['时间'].between('00:00', '10:00')) & (dep_df['市场'] == 'INT')].shape[0]
+
+# 国内出港航班数量
+domestic_flights = \
+    dep_df[(dep_df['日期'] == 2) & (dep_df['时间'].between('00:00', '10:00')) & (dep_df['市场'] == 'DOM')].shape[0]
+
+print(f"日期为2的0-10点出港航班数量: {dep_flights_2_0_10}")
+print(f"其中国际出港航班数量: {international_flights}")
+print(f"其中国内出港航班数量: {domestic_flights}")
+
+day1_int_flights = round(international_flights * 0.6)
+day1_dom_flights = round(domestic_flights * 0.6)
+print(f'第一天国际航班量：{day1_int_flights}')
+print(f'第一天国内航班量：{day1_dom_flights}')
+
+late_int_flights = arr_df[arr_df['市场'] == 'INT'].sort_values('时间', ascending=False).head(day1_int_flights)
+late_dom_flights = arr_df[arr_df['市场'] == 'DOM'].sort_values('时间', ascending=False).head(day1_dom_flights)
+late_flights = pd.concat([late_int_flights, late_dom_flights])
+late_flights['日期'] = 1
+
+arr_df = pd.concat([late_flights, arr_df]).reset_index(drop=True)
+
 # 保存到不同sheet
 with pd.ExcelWriter('pre_task2.xlsx') as writer:
     arr_df.to_excel(writer, sheet_name='到达航班', index=False)
